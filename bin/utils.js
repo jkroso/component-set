10c9440ad0ff2a13383db4f6a7398226e3268648
Jake Rosoman <jkroso@gmail.com>
2013-09-06 14:22:35 +1200

    update deps

--- bin/utils.js
+++ bin/utils.js
@@ -1,19 +1,18 @@
 
-var Promise = require('laissez-faire/full')
-  , ghtag = require('github-latest')
-  , log = require('./logger')
-  , fs = require('fs')
-
-exports.getDeps = getDeps
-exports.add = add
+var lift = require('lift-result/cps')
+var ghtag = lift(require('github-latest'))
+var Result = require('result')
+var log = require('./logger')
+var fs = require('fs')
 
 exports.parseKeyValue = function(dep){
 	// github shorthand
 	var m = /^([-.\w]+)\/([-.\w]+)(?:@([-.\w]+|\d+\.\d+\.\d+))?$/.exec(dep)
 	if (m) {
 		var key = m[1] + '/' + m[2]
-		if (m[3]) return new Promise().write([key, m[3]])
+		if (m[3]) return Result.wrap([key, m[3]])
 		return exports.latest(m[1], m[2]).then(function(tag){
+			if (tag == 'master') tag = '*'
 			return [key, tag]
 		})
 	} else {
@@ -22,16 +21,14 @@ exports.parseKeyValue = function(dep){
 }
 
 exports.latest = function(user, repo){
-	var p = new Promise
 	log.info('fetching', 'latest tag for %s/%s', user, repo)
-	ghtag(user, repo, function(e, tag){
-		if (e) log.info('error', 'unable to find the latest tag for %s/%s (%s)', user, repo, e.message)
-		p.write(tag || 'master')
+	return ghtag(user, repo).then(null, function(e){
+		log.info('error', 'unable to find the latest tag for %s/%s (%s)', user, repo, e.message)
+		return 'master'
 	})
-	return p
 }
 
-function getDeps(file){
+exports.getDeps = function(file){
 	if (!fs.existsSync(file)) throw new Error('no component.json')
 	return JSON.parse(fs.readFileSync(file, 'utf8'))
 }
@@ -43,8 +40,8 @@ function getDeps(file){
  * @param {String} url
  */
 
-function add(key, tag, opts){
-	var json = getDeps(opts.file)
+exports.add = function(key, tag, opts){
+	var json = exports.getDeps(opts.file)
 
 	// --development
 	if (opts.development) {
